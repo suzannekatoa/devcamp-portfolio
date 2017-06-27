de9f429c0c83d95064a3fbc8c474e09ddf76a385
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 15:17:38 +0000

    Save all on Portfolio-feature before migration

--- GeneratorApp/config/routes.rb
+++ GeneratorApp/config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
+  resources :categories
+  resources :guides
   resources :blogs
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
