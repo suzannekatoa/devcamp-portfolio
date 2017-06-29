e16f0c9780da2055c0571049ac2958277e5b4d78
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 17:42:26 +0000

    Implemented cutsom defaults for portfolio items

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  get 'angular-itmes', to: 'portfolio#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
