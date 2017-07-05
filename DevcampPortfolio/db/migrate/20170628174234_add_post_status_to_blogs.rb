c2b6f4eeaaa11ef7564664bb165a55e2c8bb3e86
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 14:38:20 +0000

    Implemented image defaults for portfolio items

--- DevcampPortfolio/db/migrate/20170628174234_add_post_status_to_blogs.rb
+++ DevcampPortfolio/db/migrate/20170628174234_add_post_status_to_blogs.rb
@@ -1,4 +1,4 @@
-class AddPostStatusToBlogs < ActiveRecord::Migration[5.0]
+class AddPostStatusEnumToBlogs < ActiveRecord::Migration[5.0]
   def change
     add_column :blogs, :status, :integer
   end
