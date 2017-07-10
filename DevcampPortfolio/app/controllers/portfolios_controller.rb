e0ff7de1884c6443f7ac4c72a947c510dcf6beff
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 17:20:54 +0000

    Added guest user for current_user

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,36 +1,36 @@
 class PortfoliosController < ApplicationController
   def index
-   @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
-  
- def angular
+
+  def angular
     @angular_portfolio_items = Portfolio.angular
- end 
- 
-   def new
+  end
+
+  def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
-   end
+  end
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-  
-    def edit
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-    
-    def update
+
+  def edit
     @portfolio_item = Portfolio.find(params[:id])
-    
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -38,13 +38,13 @@ class PortfoliosController < ApplicationController
         format.html { render :edit }
       end
     end
-    end
-    
-    def show
-      @portfolio_item = Portfolio.find(params[:id])
-    end
-    
-    def destroy
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
 
@@ -55,15 +55,16 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-    end
-    
-    private
-    
-    def portfolio_params
-      params.require(:portfolio).permit(:title, 
-                                        :subtitle, 
-                                        :body,
-                                        technologies_attributes: [:name]
-                                        )
-    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
 end
