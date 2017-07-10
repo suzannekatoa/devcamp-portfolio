47d465d3f94bb0535c72b135eb95917c10d4411e
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:09:07 +0000

    Added session tracker for dynamic source links

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -2,12 +2,18 @@ class PortfoliosController < ApplicationController
   def index
    @portfolio_items = Portfolio.all
   end
+  
+ def angular
+    @angular_portfolio_items = Portfolio.angular
+ end 
  
-  def new
+   def new
     @portfolio_item = Portfolio.new
-  end
-    def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    3.times { @portfolio_item.technologies.build }
+   end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -16,7 +22,7 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
-    end
+  end
   
     def edit
       @portfolio_item = Portfolio.find(params[:id])
@@ -24,6 +30,7 @@ class PortfoliosController < ApplicationController
     
     def update
     @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
