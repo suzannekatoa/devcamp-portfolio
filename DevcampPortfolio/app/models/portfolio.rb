832b868ab46fab2bd291d6a8704d2148a39e2253
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 17:07:45 +0000

    Implemented partials with local datat for portfollios form and nav bars

--- DevcampPortfolio/app/models/portfolio.rb
+++ DevcampPortfolio/app/models/portfolio.rb
@@ -1,2 +1,21 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                allow_destroy: true,
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+
+  validates_presence_of :title, :body
+  
+  mount_uploader :thumn_image, PorftolioUploader
+  mount_uploader :main_image, PortfolioUploader
+  
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  def self.by_position
+    order("position ASC")
+  end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
