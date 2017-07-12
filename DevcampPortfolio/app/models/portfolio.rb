daf0a8fe425279454a329a0cdbf0efffd439f0c6
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 16:33:34 +0000

    integrated helper metods from ActionView

--- DevcampPortfolio/app/models/portfolio.rb
+++ DevcampPortfolio/app/models/portfolio.rb
@@ -4,18 +4,26 @@ class Portfolio < ApplicationRecord
                                 allow_destroy: true,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  validates_presence_of :title, :body
 
-  mount_uploader :thumb_image, PortfolioUploader
-  mount_uploader :main_image, PortfolioUploader
+  validates_presence_of :title, :body, :thumb_image, :main_image,
+
 
   def self.angular
     where(subtitle: 'Angular')
   end
 
+   
   def self.by_position
     order("position ASC")
   end
+  
+  
+ scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+ 
+ after_initialize :set_defaults
 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  def set_defaults
+  self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+  self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
 end
