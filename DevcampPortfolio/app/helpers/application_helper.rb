3013170f2a9eb9d80f38a5345c05ee725a4efd11
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 16:46:37 +0000

    Updated styles for master layout file

--- DevcampPortfolio/app/helpers/application_helper.rb
+++ DevcampPortfolio/app/helpers/application_helper.rb
@@ -20,4 +20,54 @@ module ApplicationHelper
   def copyright_generator
     KatoaViewTool::Renderer.copyright 'Suzanne Katoa', 'All rights reserved'
   end
+   def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      ]
+   end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
+  end
 end
