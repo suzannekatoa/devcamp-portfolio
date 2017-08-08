01d81db0e94039787f10ea69c7b08e36b8b8bdd9
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 17:28:49 +0000

    Implemented custom scope for recent blogs and hide draft post from non admins

--- DevcampPortfolio/app/channels/application_cable/connection.rb
+++ DevcampPortfolio/app/channels/application_cable/connection.rb
@@ -1,7 +1,7 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identify_by :current_user
-    
+    identified_by :current_user
+
     def guest_user
       guest = GuestUser.new
       guest.id = guest.object_id
@@ -11,19 +11,19 @@ module ApplicationCable
       guest.email = "guest@user.com"
       guest
     end
-    
-    def connected
+
+    def connect
       self.current_user = find_verified_user || guest_user
       logger.add_tags 'ActionCable', current_user.email
       logger.add_tags 'ActionCable', current_user.id
     end
-    
+
     protected
-    
+
     def find_verified_user
       if verified_user = env['warden'].user
         verified_user
+      end
     end
   end
-end
 end
